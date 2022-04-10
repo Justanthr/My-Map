e9f89a2e3b52b163807663e3a340dac8c2678239
Jennika Wilson <j.r.a.112100@gmail.com>
2022-04-09 22:54:12 -0400

    foursquare is my enemy.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Get the user's coordinates:                                                              
 if (!navigator.geolocation){
     console.log('Your browser does not support geolocation feature')
-  } else {
+} else {
     navigator.geolocation.getCurrentPosition(getPosition)
-  }
-   async function getPosition(position){
+}
+   function getPosition(position){
      const lat = position.coords.latitude
      const lng = position.coords.longitude
      console.log(lat, lng)
@@ -23,15 +23,13 @@ if (!navigator.geolocation){
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(myMap)
   
-  //markers
-  const polygon = L.polygon(map, {
-  color: 'blue', 
-  fillOpacity: 0.0
-  }).addTo(myMap)
-   }
-  
+ map = myMap
+}
+
+myMap = map
+
   //forsquare 
-  async function fourSquare() {
+  async function fourSquare(business) {
     const options = {
       method: 'GET',
       headers: {
@@ -39,5 +37,35 @@ if (!navigator.geolocation){
         Authorization: 'fsq3rPwgNHEMIlHjA7eHoIJAaKzA0BX5wgcNlSyI0Xw5XcI='
       }
     };
-  
+
+    let limit = 5
+    let latitude = getPosition()
+    let longitude =  getPosition()
+	let response = await fetch(`https://api.foursquare.com/v3/places/search?query=${business}&limit=${limit}&ll=${latitude}%C${longitude}`, options)
+
+	let data = await response.text()
+	let parsedData = JSON.parse(data)
+	let businesses = parsedData.results
+	return businesses
   }
+
+  function processBusinesses(data) {
+	let businesses = data.map((element) => {
+		let location = {
+			name: element.name,
+			lat: element.geocodes.main.latitude,
+			long: element.geocodes.main.longitude
+		};
+		return location
+	})
+	return businesses
+}
+
+// business submit button
+document.getElementById('submit').addEventListener('click', async (event) => {
+	event.preventDefault()
+	let business = document.getElementById('business').value
+	let data = await fourSquare(business)
+	myMap.businesses = processBusinesses(data)
+	myMap.addMarkers()
+})
